0ff0451e4119e237b25b209df50b69517737c8dd
chathurya <nalamchathurya@gmail.com>
2026-09-14 15:48:16 +0530

    modified

--- app.py
+++ app.py
@@ -561,4 +561,4 @@ def get_player_detail(player_name: str):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
